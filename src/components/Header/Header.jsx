9b5240d8c55e46a878783e1717a436f5c0144e8e
nosfair <nosfairalchtion@gmail.com>
2024-02-16 13:50:21 +0100

    Add new components and change, delete unused code

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Header.css'
+/**
+ * Header - Components
+ * @returns {JSX.Element} - Header component JSX element.
+ */
+const Header = () => {
+    return (
+        <header>
+            <h1>HR<span>net</span></h1>
+            {window.location.pathname === "/" ? (
+                <nav>
+                    <NavLink to="/employee">
+                        View Current Employees <hr /></NavLink>
+                </nav>
+
+            ) : (
+                <nav>
+                    <NavLink to="/" >Home <hr /></NavLink>
+                </nav>
+            )}
+
+
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Button, ModalBody, ModalDialog, ModalHeader, ModalTitle } from 'react-bootstrap'
+import "./Modal.css"
+/**
+ * Modal - component - Modal Validation Form
+ * React-bootstap UI
+ * @param {function} onClose to close Modal
+ * @param {boolean} toggle to toggle Modal
+ * @returns {JSX.Element} - Modal component JSX element.
+ */
+const Modal = ({ onClose, toggle }) => {
+    return (
+        toggle && (
+            <div className='modal-container' onClick={() => onClose()}>
+                <div
+                    className="modal show"
+                >
+                    <ModalDialog>
+
+                        <ModalHeader>
+
+                            <ModalTitle>Validation</ModalTitle>
+                            <Button onClick={() => onClose()}>&#10799;</Button>
+                        </ModalHeader>
+
+
+                        <ModalBody>
+                            <p>Employee was successfully created</p>
+                        </ModalBody>
+
+
+                    </ModalDialog>
+                </div>
+            </div>
+        )
+    )
+}
+
+export default Modal
